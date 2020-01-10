a0310f67e9c3e9754cf53313a2f0bb29e9544515
Chris Gaffney <gaffneyc@gmail.com>
2020-01-10 15:16:02 -0500

    Adds internal? to ResponseError and adds documentation
    Thinking through what a ResponseError could be (using it as a value and
    an exception). Adding more documentation to make it easier to grok.

--- lib/kafka/ffi/error.rb
+++ lib/kafka/ffi/error.rb
@@ -13,16 +13,30 @@ module Kafka::FFI
   # @see rdkafka.h RD_KAFKA_RESP_ERR_*
   # @see rdkafka.h rd_kafka_resp_err_t
   class ResponseError < Error
+    # @attr code [Integer] Error code as defined by librdkafka.
     attr_reader :code
 
     def initialize(code)
       @code = code
     end
 
+    # Returns the librdkafka error constant for this error.
+    # @return [String]
     def name
       "RD_KAFKA_RESP_ERR_#{::Kafka::FFI.rd_kafka_err2name(@code)}"
     end
 
+    # Returns true when the error is from internal to librdkafka or false when
+    # the error was received from a broker or timeout.
+    #
+    # @see https://github.com/edenhill/librdkafka/blob/4818ecadee/src/rdkafka.h#L245
+    def internal?
+      code < 0
+    end
+
+    # Returns a human readable error description
+    #
+    # @return [String] Human readable description of the error.
     def to_s
       ::Kafka::FFI.rd_kafka_err2str(@code)
     end
