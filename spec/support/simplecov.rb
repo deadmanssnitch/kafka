9e7e9e8f1e5a540f8eb32196a23599747ccedb1b
Chris Gaffney <gaffneyc@gmail.com>
2021-10-21 09:20:45 -0400

    simplecov: Add client and ffi groups
    Splitting them to make it easier to focus on individual components.

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -9,6 +9,10 @@ begin
 
     # Ignore spec files since they should be fully covered
     add_filter "/spec/"
+
+    ffi = %r{lib/kafka/ffi/}
+    add_group("client") { |f| !ffi.match?(f.filename) }
+    add_group("ffi")    { |f|  ffi.match?(f.filename) }
   end
 rescue LoadError
   # SimpleCov is defined in the Gemfile but not in the gemspec and is optional
